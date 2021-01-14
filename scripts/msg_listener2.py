e20a1b44ccdf8079187adbd38d41b56eee4a671b
naka_t <test@test.com>
2021-01-14 15:08:11 +0900

    wait_for_messageを使った例を追加

---
+++ scripts/msg_listener2.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+from __future__ import print_function, unicode_literals
+import rospy
+from std_msgs.msg import String
+
+def main():
+    rospy.init_node('listener2', anonymous=True)
+
+    while not rospy.is_shutdown():
+        data = rospy.wait_for_message( "chatter", String )
+        print( data.data )
+        
+if __name__ == '__main__':
+    main()
